dc9e259637fe0093e140330cb88cc8fa18a48695
Julian Perez Ramirez <jperezr@correo.udistrital.edu.co>
2022-11-29 14:46:29 +0000

    Menu added after log i-in window

--- src/main/java/com/IngeSoft/BicisBogota/Controller/HelloWorld.java
+++ src/main/java/com/IngeSoft/BicisBogota/Controller/HelloWorld.java
@@ -6,19 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
     @GetMapping("/menu")
-    public String getMessage(){
-        return "<h1 th:inline=\"text\">Hello [[${#httpServletRequest.remoteUser}]]!</h1>"
-        + "<form th:action=\"@{./logout}\" method=\"post\">"
-        + "<input type=\"submit\" value=\"Sign Out\"/>"
-        + "</form>";    
-        //return "Hello World <p>"
-        //+"<hr/>"
-        //+ "<ul>"
-        //+ "<li><a href=\"./users\">Users</a></li>"
-        //+ "<li><a href=\"./bicycles\">Bicycles</a></li>"
-        //+ "<li><a href=\"./locations\">Locations</a></li>"
-        //+ "<li><a href=\"./arrives\">Arrives</a></li>"
-        //+ "<li><a href=\"./rents\">Rents</a></li>"
-        //+ "</ul>";
+    public String getMessage(){    
+        return "Hello World <p>"
+        +"<hr/>"
+        +"<ul>"
+        + "<li><a href=\"./users\">Users</a></li>"
+        + "<li><a href=\"./bicycles\">Bicycles</a></li>"
+        + "<li><a href=\"./locations\">Locations</a></li>"
+        + "<li><a href=\"./arrives\">Arrives</a></li>"
+        + "<li><a href=\"./rents\">Rents</a></li>"
+        + "</ul>";
     }
 }
